ec2574f986560de36b8aabeacafd44d623e46b79
Inge Seim <inge.seim@gmail.com>
2014-03-23 21:57:58 +1000

    Update FusionMapCutoff.pl

--- FusionMapCutoff.pl
+++ FusionMapCutoff.pl
@@ -1,35 +1,79 @@
 #!/usr/bin/perl 
-use strict;
+# use strict;
 use Term::ANSIColor;
+use List::Util qw/max/;
 
 ################################################################################################################################################################
 # FusionMapCutoff.pl - this script does something awful.
 # By Inge Seim
-# 1/2014
+# 4/2014
 ################################################################################################################################################################
 print color 'blue' ;
 print "# ###############################################################################" . "\n" ;
 print color 'reset';
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# need a hash for later
+#
+# in contast to array, not number indexed, but uses names/keys
+my %goodhits ; # declare emtpy hash
+# individual elements are scalars/$, just like arrays
+# hashes are unordered lists
+# if you my re-declare a value later, it will be overwritten
+# e.g. the count of a FusionGene --which will prove useful later
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# this is used to simply count the # of samples
+#
+my $realcount = "" ;
+# the number of samples. Later you want to only allow fusion that has the same count as this. 
+my $dir = './data';
+my @files = <$dir/*.txt>;
+$realcount = @files;
+# print $realcount . " is the numbers of samples examined" . "\n" ;
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+# TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+# set the Threshold
 print color 'red' ;
+
+
+# OPTION 1
+# use this if you are OK with manually entering your threshold
 # add your threshold (percentage. e.g. 50 for 50%)
 print "Please enter your desired chimaera/fusion transcript cutoff" . "\n" ;
 print "e.g. 50 for 50%: ";
 my $percentagecutoff = <STDIN>; 
 chomp $percentagecutoff; # Get rid of newline character at the end
 exit 0 if ($percentagecutoff eq ""); # If empty string, exit.
+
+# OPTION 2
+# use this if you do not want to enter the value manually each time
+# my $percentagecutoff = "70" ; # 
+
 print color 'reset';
 system("clear");
+# TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 
-################################################################################################################################################################
-system("mkdir ./output");
+
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# get rid of files from any previous runs
+system("rm ./output.txt"); 
 system("rm ./temp.txt");
 system("rm ./fusiongeneswithcount.txt");
 system("rm ./fusionsAboveThreshold.txt");
-
 system("clear");
-################################################################################################################################################################
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+
+
+
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# HERE COMES THE PARSING OF THE DATA FILES
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 my $fusiongeneswithcount = "" ;
 
 # want to count the # of samples!
@@ -46,7 +90,6 @@ my @fusionsraw = "" ;
 # delete element 0
 splice @fusionsraw, 0, 1;
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 my @fusionsrawunique = "" ;
@@ -117,24 +160,13 @@ open (FILE, ">>./temp.txt") ;  #open for write, append
 print FILE $printout  . "\n" ;          
 # print FILE "@uniq_array"  . "\n" ;    
          close(FILE) ;
- print $printout . "\n" ; 
+# print $printout . "\n" ; 
  
 }
 
  # close the loop. Group 1, e.g. luminal is ready!
 # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# this is used to simply count the # of samples
-#
-my $realcount = "" ;
-foreach my $keys (keys %counts) {
-# the number of samples. Later you want to only allow fusion that has the same count as this. 
-# print "$counts{$keys}" . " is the number of samples examined in group 1" . "\n" . "\n" ;
-$realcount = "$counts{$keys}" ;
-# print $realcount . " i bank" . "\n" ;
-}
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # OK, now open 
 local $/=undef;  # give me the whole file at once!
@@ -173,12 +205,11 @@ my $mergedelements1INDEX = $fusiongeneswithcountarray[$index];
 my ($FusionGene, $FusionGeneCount) = split("\t", $mergedelements1INDEX) ;
 
 my $acceptablecutoff = ($realcount * $percentagecutoff)/100 ;
-#@ print $acceptablecutoff . " is the value after correcting for % threshold" . "\n" ;
-# my $targetcount = $realcount ;
-my $targetcount = $acceptablecutoff ;
-if ($FusionGeneCount >= $targetcount) {
+# print $acceptablecutoff . " is the acceptable cut-off" . "\n" ;
+# print $FusionGeneCount . " is the FusionGeneCount" . "\n"  ;
+my $targetcount = $acceptablecutoff ;  
 
-
+if ($FusionGeneCount >= $targetcount) {      
 print color 'yellow' ;
 print "###############################################################################################################################################################" . "\n" ;
 print color 'reset';
@@ -193,21 +224,89 @@ print $FusionGeneCount ;
 print color 'reset';
 print  "\* and has reached the accepted threshold and is considered overrepresented in the group" . "\n" ;
 
-}         
 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# feed into a Perl hash
+$goodhits{$FusionGene} = $FusionGeneCount;
+
+} 
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # end of loop for EACH sample
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+}
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# END OF PARSING
+
+
+
+
+
+
+
+
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# NOW OUTPUT FUSION GENES THAT PASSED YOUR THRESHOLD!
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+while ( my ($key, $value) = each(%goodhits) ) {
+print "###############################################################################################################################################################" . "\n" ;
+# open(OUT,">>myFileWithDuplicates.txt");   # if you want to output all values 
+#        print OUT "$key => $value\n";     
+ 
+        print "$key => $value\n";
+print color 'reset';
+print  "\* and has reached the accepted threshold and is considered overrepresented in the group" . "\n" ;
+
+    }
+
+# print  @values_my_from_my_hash . "are the keys from the hash" ;
 
 }
+# only keep the highest hash value
+# shamelessly stolen from http://stackoverflow.com/questions/12639688/finding-duplicate-hash-keys-in-perl-and-finding-maximum-value-among-them user dan1111
+unless (exists $goodhits{$key} and $goodhits{$key} >= $value)
+{
+   $goodhits{$key} = $value;
 }
+open(OUT,">>output.txt");
+# then save
+print OUT "Fusion RNA" . "\t" . "% of samples" . "\n" ;
+foreach my $key (sort keys %goodhits) {
+my $fusioncount = $goodhits{$key} ; 
+# print $fusioncount . "\n" ;
+# print $realcount . "\n" ;
+my $match = ($fusioncount /= $realcount) * 100 ;
+$match = sprintf("%0.1f", $match); # want a neater number. See http://www.tizag.com/perlT/perloperators.php
+# print $match . "\n" ;
+   print OUT $key . "\t" . $match . "\n";
+#   print OUT $key . "\t" . $goodhits{$key} . "\n";
+
+}
+close(OUT);
+
+# delete the second line of the file 
+system("sed -i\".bac\" '2d' output.txt");
+system("rm ./output.txt.bac"); # for the paranoid: kept the backup option
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
 print color 'yellow' ;
 print "###############################################################################################################################################################" . "\n" ;
 print color 'reset';
 
+print "\n" ;
+print "\n" ;
 print color 'red' ;
-print "THIS IS A LIST OF CHIMAERIC TRANSCRIPTS ABOVE YOUR THRESHOLD, WHICH IS " . $percentagecutoff . "\%" . "\n" ;
+print "THE TEXT ABOVE IS A LIST OF CHIMAERIC TRANSCRIPTS ABOVE YOUR THRESHOLD, WHICH IS " . $percentagecutoff . "\%" . "\n" ;
+print "\n" ;
 print color 'reset';
-
+print color 'white' ;
+print "THE FILE output.txt CONTAINS YOUR LIST OF FUSION TRANSCRIPTS" . "\n" ;
+print color 'reset';
+print "\n" ;
 
 system("rm ./temp.txt");
 system("rm ./fusiongeneswithcount.txt");
+
+# ••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# this is the End
+
